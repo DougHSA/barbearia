c222517e99234adb31191e6f7e6b8c12c81f2691
DougHSA <DougHSA97@gmail.com>
2024-02-03 18:56:14 -0300

    fix: corrigir erro de tipagem

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -30,7 +30,7 @@ const ServiceItem = ({service, barbershop, isAuthenticated}:ServiceItemProps) =>
     const [hour,setHour] = useState<String | undefined>();
     const [submitIsLoading, setSubmitIsLoading] = useState(false);
     const [sheetIsOpen, setSheetIsOpen] = useState(false);
-    const [dayBookings, setDayBookings] = useState<Booking[]>([]);
+    const [dayBookings, setDayBookings] = useState<any[]>([]);
 
 
     useEffect(()=>{
@@ -119,82 +119,96 @@ const ServiceItem = ({service, barbershop, isAuthenticated}:ServiceItemProps) =>
         })
     },[date, dayBookings]);
 
-    return ( 
-        <Card>
-            <CardContent className="p-3 w-full">
-                <div className="flex gap-4 items-center w-full">
-                    <div className="relative min-h-[110px] min-w-[110px] max-h-[110px] max-w-[110px]">
-                    <Image src={service.imageUrl} fill alt={service.name} style={{
-                        objectFit:'contain',
-                    }} className="rounded-lg"/>
-                    </div>
-                    <div className="flex flex-col w-full">
-                        <h2 className="font-bold">{service.name}</h2>
-                        <p className="text-sm text-gray-400">{service.description}</p>
-                        <p className="text-sm text-gray-400">{`${service.timeSpend} minutos`}</p>
+    return (
+			<Card>
+				<CardContent className="p-3 w-full">
+					<div className="flex gap-4 items-center w-full">
+						<div className="relative min-h-[110px] min-w-[110px] max-h-[110px] max-w-[110px]">
+							<Image
+								src={service.imageUrl}
+								fill
+								alt={service.name}
+								style={{
+									objectFit: "contain",
+								}}
+								className="rounded-lg"
+							/>
+						</div>
+						<div className="flex flex-col w-full">
+							<h2 className="font-bold">{service.name}</h2>
+							<p className="text-sm text-gray-400">{service.description}</p>
+							<p className="text-sm text-gray-400">{`${service.timeSpend} minutos`}</p>
 
-                        <div className="flex items-center justify-between mt-3">
-                            <p className="text-primary text-sm font-bold">
-                                {Intl.NumberFormat(
-                                'pt-BR', 
-                                {style: 'currency', currency: 'BRL'}
-                                ).format(Number(service.price))}
-                            </p>
-                            <Sheet open={sheetIsOpen} onOpenChange={setSheetIsOpen}>
-                                <SheetTrigger asChild>
-                                    <Button onClick={handleBookingClick} variant="secondary">Reservar</Button>
+							<div className="flex items-center justify-between mt-3">
+								<p className="text-primary text-sm font-bold">
+									{Intl.NumberFormat("pt-BR", {
+										style: "currency",
+										currency: "BRL",
+									}).format(Number(service.price))}
+								</p>
+								<Sheet open={sheetIsOpen} onOpenChange={setSheetIsOpen}>
+									<SheetTrigger asChild>
+										<Button onClick={handleBookingClick} variant="secondary">
+											Reservar
+										</Button>
+									</SheetTrigger>
+									<SheetContent className="p-0 overflow-y-auto">
+										<SheetHeader className="text-left px-5 py-4 border-b border-solid border-secondary">
+											<SheetTitle>Fazer Reserva</SheetTitle>
+										</SheetHeader>
+										<div className="pt-3 pb-6 w-full">
+											<Calendar
+												mode="single"
+												selected={date}
+												onSelect={handleDateClick}
+												className=""
+												fromDate={addDays(new Date(), 1)}
+												locale={ptBR}
+												styles={{
+													head_cell: {
+														width: "100%",
+														textTransform: "capitalize",
+													},
+													cell: {
+														width: "100%",
+													},
+													button: {
+														width: "100%",
+													},
+													nav_button_next: {
+														width: "32px",
+														height: "32px",
+													},
+													nav_button_previous: {
+														width: "32px",
+														height: "32px",
+													},
+													caption: {
+														textTransform: "capitalize",
+													},
+													month: {
+														width: "100%",
+													},
+												}}
+											/>
+										</div>
 
-                                </SheetTrigger>
-                                <SheetContent className="p-0 overflow-y-auto">
-                                    <SheetHeader className="text-left px-5 py-4 border-b border-solid border-secondary">
-                                        <SheetTitle>Fazer Reserva</SheetTitle>
-                                    </SheetHeader>
-                                        <div className="pt-3 pb-6 w-full">
-                                            <Calendar mode="single" selected={date} onSelect={handleDateClick} className="" fromDate={addDays(new Date(),1)} locale={ptBR} styles={{
-                                                head_cell:{
-                                                    width:"100%",
-                                                    textTransform:"capitalize",
-                                                },
-                                                cell:{
-                                                    width:"100%",
-                                                },
-                                                button:{
-                                                    width:"100%",
-                                                    },
-                                                nav_button_next:{
-                                                    width:"32px",
-                                                    height:"32px"
-                                                },
-                                                nav_button_previous:{
-                                                    width:"32px",
-                                                    height:"32px"
-                                                },
-                                                caption:{
-                                                    textTransform:"capitalize",
-                                                },
-                                                month:{
-                                                    width:"100%",
-                                                }
+										{date && (
+											<div className="flex gap-3 overflow-x-auto py-6 px-5 border-t border-solid border-secondary [&::-webkit-scrollbar]:hidden">
+												{timeList.map((time) => (
+													<Button
+														onClick={() => handleHourClick(time)}
+														variant={hour === time ? "default" : "outline"}
+														className="rounded-full"
+														key={time}
+													>
+														{time}
+													</Button>
+												))}
+											</div>
+										)}
 
-                                            }}/>
-                                        </div>
-
-                                    {date &&(
-                                        <div className="flex gap-3 overflow-x-auto py-6 px-5 border-t border-solid border-secondary [&::-webkit-scrollbar]:hidden">
-                                            {timeList.map((time)=>(
-                                                <Button 
-                                                onClick={() => handleHourClick(time)}
-                                                variant={hour === time ? "default" : "outline"} 
-                                                className="rounded-full" 
-                                                key={time}
-                                                >
-                                                    {time}
-                                                </Button>
-                                            ))}
-                                        </div>
-                                    )}
-
-                                    <div className="py-6 px-5 border-t border-solid border-secondary">
+                                            <div className="py-6 px-5 border-t border-solid border-secondary">
                                         <Card>
                                             <CardContent className="p-3 gap-3 flex flex-col">
                                                 <div className="flex justify-between">
@@ -206,52 +220,62 @@ const ServiceItem = ({service, barbershop, isAuthenticated}:ServiceItemProps) =>
                                                         {style: 'currency', currency: 'BRL'}
                                                         ).format(Number(service.price))}
                                                     </h3>
-                                                </div>
+                                        </div>
 
-                                                {date &&(
-                                                    <div className="flex justify-between">
-                                                        <h3 className="text-gray-400 text-sm">Data</h3>
-                                                        <h4 className="text-sm capitalize">{format(date,"dd 'de' MMMM",{
-                                                            locale:ptBR
-                                                        })}</h4>
-                                                    </div>
-                                                )}     
 
-                                                {hour &&(
-                                                    <div className="flex justify-between">
-                                                        <h3 className="text-gray-400 text-sm">Horário</h3>
-                                                        <h4 className="text-sm capitalize">{hour}</h4>
-                                                    </div>
-                                                )}
+													{date && (
+														<div className="flex justify-between">
+															<h3 className="text-gray-400 text-sm">Data</h3>
+															<h4 className="text-sm capitalize">
+																{format(date, "dd 'de' MMMM", {
+																	locale: ptBR,
+																})}
+															</h4>
+														</div>
+													)}
 
-                                                <div className="flex justify-between">
-                                                    <h3 className="text-gray-400 text-sm">Tempo</h3>
-                                                    <h4 className="text-sm capitalize">{`${service.timeSpend} minutos`}</h4>
-                                                </div>
+													{hour && (
+														<div className="flex justify-between">
+															<h3 className="text-gray-400 text-sm">Horário</h3>
+															<h4 className="text-sm capitalize">{hour}</h4>
+														</div>
+													)}
 
-                                                <div className="flex justify-between">
-                                                    <h3 className="text-gray-400 text-sm">Barbearia</h3>
-                                                    <h4 className="text-sm capitalize">{barbershop.name}</h4>
-                                                </div>
+													<div className="flex justify-between">
+														<h3 className="text-gray-400 text-sm">Tempo</h3>
+														<h4 className="text-sm capitalize">{`${service.timeSpend} minutos`}</h4>
+													</div>
 
-                                                <SheetFooter>
-                                                    <Button onClick={handleBookingSubmit} disabled={!hour || !date || submitIsLoading} className="px-5">
-                                                        {submitIsLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-                                                        Confirmar Reserva
-                                                    </Button>
-                                                </SheetFooter>        
-                                            </CardContent>
-                                        </Card>
-                                    </div>
-                                </SheetContent>
+													<div className="flex justify-between">
+														<h3 className="text-gray-400 text-sm">Barbearia</h3>
+														<h4 className="text-sm capitalize">
+															{barbershop.name}
+														</h4>
+													</div>
 
-                            </Sheet>
-                        </div>
-                    </div>
-                </div>
-            </CardContent>
-        </Card>
-     );
+													<SheetFooter>
+														<Button
+															onClick={handleBookingSubmit}
+															disabled={!hour || !date || submitIsLoading}
+															className="px-5"
+														>
+															{submitIsLoading && (
+																<Loader2 className="mr-2 h-4 w-4 animate-spin" />
+															)}
+															Confirmar Reserva
+														</Button>
+													</SheetFooter>
+												</CardContent>
+											</Card>
+										</div>
+									</SheetContent>
+								</Sheet>
+							</div>
+						</div>
+					</div>
+				</CardContent>
+			</Card>
+		);
 }
  
 export default ServiceItem;
